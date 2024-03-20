f8a0fc7bde8ee826c84940c43a992fd1c68fecaa
y1wtt <awataki1206@gmail.com>
2024-03-20 17:08:32 +0900

    swap x-y

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ const createHeader = (parent:HTMLDivElement) =>{
     const rowEl = document.createElement("div")
     rowEl.className = 'flex flex-row'
     const colorEl = document.createElement("div");
-    colorEl.className = 'cell'
+    colorEl.className = 'cell indicator'
     rowEl.appendChild(colorEl)
-    for (let j = 0; j < 10; j++) {
+    for (let j = 0; j < 11; j++) {
         const colorEl = document.createElement("div");
-        colorEl.className = 'cell indicator'
-        colorEl.innerHTML = `L${j*10}`
+        colorEl.className = 'cell indicator indicator-x'
+        colorEl.innerHTML = `S${j*10}`
         rowEl.appendChild(colorEl)
     }
     parent.appendChild(rowEl)
@@ -19,17 +19,17 @@ const createColorMatrix = (e:String) => {
     const entryPoint = document.querySelector<HTMLDivElement>("#matrix-entry-point")
     entryPoint!.innerHTML = ''
     createHeader(entryPoint!)
-    for (let i = 0; i < 11; i++) {
+    for (let i = 0; i < 10; i++) {
         const rowEl = document.createElement("div")
         rowEl.className = 'flex flex-row'
         const indicatorEl = document.createElement("div");
-        indicatorEl.className = 'cell indicator'
-        indicatorEl.innerHTML = `S${i * 10}`
+        indicatorEl.className = 'cell indicator indicator-y'
+        indicatorEl.innerHTML = `B${i * 10}`
         rowEl.appendChild(indicatorEl)
         for (let j = 0; j < 11; j++) {
             const colorEl = document.createElement("div");
             colorEl.className = 'cell color-element'
-            colorEl.style.backgroundColor = `hsl(${InputToHue(e)},${i * 10}%,${j*10}%)`
+            colorEl.style.backgroundColor = `hsl(${InputToHue(e)},${j * 10}%,${i*10}%)`
             rowEl.appendChild(colorEl)
         }
         entryPoint!.appendChild(rowEl)
