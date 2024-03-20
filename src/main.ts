92a67c6d17f2cf99b7cf28fd92967b1876f628a7
y1wtt <awataki1206@gmail.com>
2024-03-20 20:17:37 +0900

    fix SB calc

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,10 @@ const createHeader = (parent:HTMLDivElement) =>{
     const colorEl = document.createElement("div");
     colorEl.className = 'cell indicator'
     rowEl.appendChild(colorEl)
-    for (let j = 0; j < 11; j++) {
+    for (let j = 0; j < 10; j++) {
         const colorEl = document.createElement("div");
         colorEl.className = 'cell indicator indicator-x'
-        colorEl.innerHTML = `S${j*10}`
+        colorEl.innerHTML = `B${(j + 1) *10}`
         rowEl.appendChild(colorEl)
     }
     parent.appendChild(rowEl)
@@ -25,12 +25,14 @@ const createColorMatrix = (e:String) => {
         rowEl.className = 'flex flex-row'
         const indicatorEl = document.createElement("div");
         indicatorEl.className = 'cell indicator indicator-y'
-        indicatorEl.innerHTML = `B${i * 10}`
+        indicatorEl.innerHTML = `S${(i + 1) * 10}`
         rowEl.appendChild(indicatorEl)
-        for (let j = 0; j < 11; j++) {
+        for (let j = 0; j < 10; j++) {
             const colorEl = document.createElement("div");
             colorEl.className = 'cell color-element'
-            colorEl.style.backgroundColor = `hsl(${InputToHue(e)},${j * 10}%,${i*10}%)`
+            const S = 5 + Math.max(i,j) * 10
+            const B = 5 + j*(5.5+(j-i)/2)
+            colorEl.style.background = `hsl(${InputToHue(e)}deg,${S}%,${B}%)`
             rowEl.appendChild(colorEl)
         }
         entryPoint!.appendChild(rowEl)
